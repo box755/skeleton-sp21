442004744bf13e5eeafc0e09e92d02f0d6591ecf
box755 <kk20031022@gmail.com>
2024-08-03 01:54:21 +0800

    Debug

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -9,57 +9,50 @@ import java.util.ArrayDeque;
 
 public class TestArrayDequeEC {
     @Test
-    public void randomizeTest(){
+    public void randomizeTest() {
         StudentArrayDeque<Integer> buggyDeque = new StudentArrayDeque<>();
         ArrayDeque<Integer> correctDeque = new ArrayDeque<>();
 
         int N = 200;
 
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             int operationNum = StdRandom.uniform(0, 4);
-            //addFirst
-            if(operationNum == 0){
+            // addFirst
+            if (operationNum == 0) {
                 int number = StdRandom.uniform(0, 100);
                 correctDeque.addFirst(number);
                 buggyDeque.addFirst((number));
-                System.out.println("addFirst(" + number + ")");
                 Integer buggySize = buggyDeque.size();
                 Integer correctSize = correctDeque.size();
-                assertEquals("The size after removeFirst should be " + correctSize + " rather than " + buggySize ,buggySize, correctSize);
+                assertEquals("The size after addFirst(" + number + ") should be " + correctSize + " rather than " + buggySize, buggySize, correctSize);
             }
-            //addLast
-            else if(operationNum == 1){
+            // addLast
+            else if (operationNum == 1) {
                 int number = StdRandom.uniform(0, 100);
                 correctDeque.addLast(number);
                 buggyDeque.addLast((number));
-                System.out.println("addLast(" + number + ")");
                 Integer buggySize = buggyDeque.size();
                 Integer correctSize = correctDeque.size();
-                assertEquals("The size after removeLast should be " + correctSize + " rather than " + buggySize ,buggySize, correctSize);
+                assertEquals("The size after addLast(" + number + ") should be " + correctSize + " rather than " + buggySize, buggySize, correctSize);
             }
-            //remove First
-            else if(operationNum == 2){
-                if(correctDeque.isEmpty() || buggyDeque.isEmpty()){
+            // removeFirst
+            else if (operationNum == 2) {
+                if (correctDeque.isEmpty() || buggyDeque.isEmpty()) {
                     continue;
                 }
                 Integer removedBuggy = buggyDeque.removeFirst();
                 Integer removeCorrect = correctDeque.removeFirst();
-                assertEquals(removedBuggy, removeCorrect);
-                System.out.println("removeFirst() " + removeCorrect);
+                assertEquals("The value removed by removeFirst() should be " + removeCorrect + " rather than " + removedBuggy, removedBuggy, removeCorrect);
             }
-            //remove Last
-            else if(operationNum == 3){
-                if(correctDeque.isEmpty() || buggyDeque.isEmpty()){
+            // removeLast
+            else if (operationNum == 3) {
+                if (correctDeque.isEmpty() || buggyDeque.isEmpty()) {
                     continue;
                 }
                 Integer removedBuggy = buggyDeque.removeLast();
                 Integer removeCorrect = correctDeque.removeLast();
-                assertEquals(removedBuggy, removeCorrect);
-                System.out.println("removeLast() " + removeCorrect);
-
+                assertEquals("The value removed by removeLast() should be " + removeCorrect + " rather than " + removedBuggy, removedBuggy, removeCorrect);
             }
         }
-
-
     }
 }
