6621b135cf1a286903b7ea9f313828e6852f05a7
box755 <kk20031022@gmail.com>
2024-10-19 22:14:52 +0800

    Finished lab12

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,4 +12,85 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
-}
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 40;
+    private static Random random  = new Random(234234);
+
+    public static void main(String[] args){
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        addHexagon(3, 30, 0, world);
+        addHexagon(3, 20, 6, world);
+        addHexagon(3, 20, 12, world);
+        addHexagon(3, 20, 18, world);
+
+
+
+
+        addHexagon(3, 25, 3, world);
+        addHexagon(3, 25, 9, world);
+        addHexagon(3, 25, 15, world);
+        addHexagon(3, 25, 21, world);
+
+        addHexagon(3, 30, 6, world);
+        addHexagon(3, 30, 12, world);
+        addHexagon(3, 30, 18, world);
+        addHexagon(3, 30, 6, world);
+        addHexagon(3, 30, 24, world);
+
+        addHexagon(3, 35, 3, world);
+        addHexagon(3, 35, 9, world);
+        addHexagon(3, 35, 15, world);
+        addHexagon(3, 35, 21, world);
+
+        addHexagon(3, 40, 6, world);
+        addHexagon(3, 40, 12, world);
+        addHexagon(3, 40, 18, world);
+
+
+
+
+
+
+
+        ter.renderFrame(world);
+
+    }
+
+    public static void addHexagon(int s, int posX, int posY, TETile[][] world) {
+        TETile tile = TETile.colorVariant(Tileset.WALL, 1000, 33242, 24343 , random);
+        // 下半部分
+        for (int y = 0; y < s; y++) {
+            // 每一行的起始X位置 = posX - y
+            int startX = posX - y;
+            // 每一行的终止X位置 = posX + s + y - 1
+            int endX = posX + s + y;
+            for (int x = startX; x < endX; x++) {
+                world[x][posY + y] = tile; // 使用Tileset的WALL来填充
+            }
+        }
+
+        // 上半部分
+        for (int y = 0; y < s; y++) {
+
+            // 每一行的起始X位置 = posX - (s - y - 1)
+            int startX = posX + (y - s + 1);
+            // 每一行的终止X位置 = posX + s + (s - y - 2)
+            int endX = posX + s + (s - y - 1);
+            for (int x = startX; x < endX; x++) {
+                world[x][posY + s + y] = tile;
+            }
+        }
+    }
+
+
+    }
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -4,22 +4,21 @@
 After looking at your own implementation, consider the implementation provided near the end of the lab.
 How did your implementation differ from the given one? What lessons can be learned from it?**
 
-Answer:
-
+Answer: The given one has more help method. 
 -----
 
 **Can you think of an analogy between the process of tessellating hexagons and randomly generating a world using rooms and hallways?
 What is the hexagon and what is the tesselation on the Project 3 side?**
 
-Answer:
+Answer: The room, hallway or door is the hexagon, and the map is the tesselation.
 
 -----
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
-Answer:
+Answer: The methods that store certain tile in the world array.
 
 -----
 **What distinguishes a hallway from a room? How are they similar?**
 
-Answer:
+Answer: Hallway is a thin space, while room is a enclosed space with a door to the hallway.
